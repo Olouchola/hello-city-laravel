732151451038be1293e8f40ac5dab15bb91ef122
Olouchola <83403006+Olouchola@users.noreply.github.com>
2021-05-12 03:12:47 +0100

    Remove duplicate using app

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,16 +1,7 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('app')
 
-        <title> Us| Laravel</title>
-    </head>
-    <body>
+@section('content')
         <p>Bonjour &hearts; les amis</p>
         <a href="/">Page d'accueil</a>
         <p>It's currently {{ date('H:i A') }}.</p>
-
-        <footer>&copy; Copyright {{date('Y')}} &middot; <a href="/about-us">About Us</a></footer>
-    </body>
-</html>
+@endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,8 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('app')
 
-        <title>Us|Laravel</title>
-    </head>
-    <body>
-        <h2>welcome to Laravel</h2>
-        <p>It's currently {{ date('H:i A') }}.</p>
 
-        <footer>&copy; Copyright {{date('Y')}} &middot; <a href="/about-us">About Us</a></footer>
-    </body>
-</html>
+@section('content')
+    <h2>welcome to Laravel</h2>
+    <p>It's currently {{ date('H:i A') }}.</p>
+@endsection
+
